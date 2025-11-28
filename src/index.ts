7709aa6e634fc1e640331b3097d78bbdc24d5786
Richardboluwatife <richardfadareboluwatife@gmail.com>
2025-11-28 14:56:34 +0100

    cors commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
-
+import cors from "cors";
 import authRouter from "./authRouter";
 import taskRouter from "./taskRouter";
 
 const app = express();
 const PORT = 2000;
+
+// -------------------------------
+// CORS setup
+// -------------------------------
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true); // allow requests with no origin (Postman, curl)
+        if (/^http:\/\/localhost:\d+$/.test(origin)) return callback(null, true); // allow any localhost port
+        callback(new Error("Not allowed by CORS")); // block other origins
+    },
+    credentials: true,
+}));
 
 app.use(express.json());
 
